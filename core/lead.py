6909bb53b214fe1d63a254b14534d807fd8ff34d
kiran kumar kota <kirakumar023@gmail.com>
2022-03-19 20:22:34 +0530

    Full Lead feature source code changes available.

---
+++ core/lead.py
@@ -0,0 +1,18 @@
+##
+# Lead feature functionalitites 
+##
+
+
+# Create Lead.
+def create_lead():
+	print "Created Lead successfully."
+
+
+# Update Lead.
+def update_lead();
+	print "Lead Updated successfully."
+
+
+# Delete Lead.
+def delete_lead();
+	print "Lead Deleted successfully."
